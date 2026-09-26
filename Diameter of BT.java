5b52fb6f325a279f19a07c35104312839f7e1d77
Saksham Singh <2005sakshamsingh@gmail.com>
2026-09-26 18:35:25 +0530

    Diameter of BT

---
+++ Diameter of BT.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int max = 0;
+   
+    public int diameterOfBinaryTree(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        Diameter(root);
+        return max;
+    }
+
+    private int Diameter(TreeNode node){
+        if(node==null) {
+            return 0;
+        }
+        int left = Diameter(node.left);
+        int right = Diameter(node.right);
+
+        max = Math.max(max, left + right);
+
+        return 1 + Math.max(left, right);
+    }
+}
